2fb67a6fe87d6c1304bf3d873ac8e2dbbb54fb91
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-01-03 16:46:31 +0300

    dodelat hiperbol. parabol kinda works

--- Laba_2_Medvedik_ur_prodolnih_koleb/Source.cpp
+++ Laba_2_Medvedik_ur_prodolnih_koleb/Source.cpp
@@ -6,16 +6,125 @@
 
 using namespace std;
 
+//  PARABOLICH
+//  
+//using namespace std;
+//const int N = 6, K = 10;
+//const int Nm = N - 1;
+//double L = 3.14, T = 5, a = 0.1,
+//h = L / N, teta = T / K;
+//double U_k[N + 1], /*U_kLANA[N + 1],*/ U_kp1[N + 1];   //матрицы для явной схемы
+//double A[Nm][Nm + 1];
+//double sigma = a * a * teta / h / h;
+//
+////alpha - коэффициент перед производной в левом условии
+////betha - коэффициент перед функцией в левом условии
+//double alpha = 1, betha = 1;
+////gamma - коэффициент перед производной в правом условии
+////delta - коэффициент перед функцией в правом условии
+//double gamma = 1, delta = 1;
+//
+//double U(int j) {
+//	return teta * a * a * (U_k[j + 1] - 2 * U_k[j] + U_k[j - 1]) / h / h + U_k[j];
+//}
+//
+////начальное условие
+//double begU(double x) {
+//	return sin(x);
+//}
+////правое граничное условие
+//double RightU(double t) {
+//	return 0;
+//}
+////левое граничное условие
+//double LeftU(double t) {
+//	//return alpha*(U_kp1[1]-U_kp1[0]
+//	return 0;
+//}
+//
+//double AnalitRew(double x,double t) {
+//
+//	return exp(-a * t) * sin(x);
+// }
+//
+//
+////void Gauss(int k, double Matrix[Nm][Nm + 1]) {
+////	if (Matrix[k][k] != 1) {
+////		double T = Matrix[k][k];
+////		for (int j = k; j < Nm + 1; j++) {//нормирование строки
+////			Matrix[k][j] = Matrix[k][j] / T;
+////		}
+////	}
+////	for (int i = 0; i < Nm; i++) { //проходим по столбцу
+////		if ((Matrix[i][k] != 0) & (i != k)) {
+////			double T = Matrix[i][k];
+////			Matrix[i][k] = 0;
+////			for (int j = k + 1; j < Nm + 1; j++) { //проходим по двум строкам и вычитаем их
+////				Matrix[i][j] -= Matrix[k][j] * T;
+////			}
+////		}
+////	}
+////	if (k < Nm - 1) {
+////		Gauss(k + 1, Matrix);
+////	}
+////}
+//
+//void vyvod(double Matr[Nm][Nm + 1]) {
+//	for (int i = 0; i < Nm; i++) {
+//		for (int j = 0; j < Nm + 1; j++) {
+//			cout << Matr[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//}
+//
+//void yavnaya() {
+//	cout << "sigma: " << sigma << endl;
+//	for (int i = 0; i <= N; i++) {
+//		U_k[i] = begU(i * h);
+//	}
+//	U_k[0] = LeftU(0);
+//	U_k[N] = RightU(0);
+//
+//	for (int k = 1; k <= K; k++) {
+//		for (int j = 1; j <= N - 1; j++) {
+//			U_kp1[j] = U(j);
+//		}
+//		for (int j = 1; j <= N - 1; j++) {
+//			U_k[j] = U_kp1[j];
+//		}
+//		//U_kLANA[j] = AnalitRew(j*h, T);
+//
+//	}
+//
+//	cout << "yavnaya: " << endl;
+//	ofstream f1("parabolich_yavn.txt", ios_base::trunc);
+//	for (int i = 0; i <= N; i++) {
+//		f1 << i * h << " " << U_k[i] << " " << AnalitRew(i * h, T) << endl;
+//		cout << i * h << " " << U_k[i] << " " << AnalitRew(i * h, T) << endl;
+//	}
+//	f1.close();
+//
+//}
+//
 
+
+
+////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
-const int N = 6, K = 10;
+const int N = 40, K = 100;
 const int Nm = N - 1;
-double L = 3.14, T = 5, a = 0.1,
+double L = 5, T = 5,
 h = L / N, teta = T / K;
-double U_k[N + 1], /*U_kLANA[N + 1],*/ U_kp1[N + 1];   //матрицы для явной схемы
+double a = 1, b = 1, c = 1;
+double U_k[N + 1], U_kp1[N + 1], U_km1[N + 1];   //матрицы для явной схемы
 double A[Nm][Nm + 1];
-double sigma = a * a * teta / h / h;
+double sigma = a * a * teta * teta / h / h;
+
+/*
+U_tt=a^2*U_xx+b*U_x+c*U+f(x,t)
+*/
 
 //alpha - коэффициент перед производной в левом условии
 //betha - коэффициент перед функцией в левом условии
@@ -24,13 +133,23 @@ double alpha = 1, betha = 1;
 //delta - коэффициент перед функцией в правом условии
 double gamma = 1, delta = 1;
 
-double U(int j) {
-	return teta * a * a * (U_k[j + 1] - 2 * U_k[j] + U_k[j - 1]) / h / h + U_k[j];
+double f(double x, double t) {
+	//return 0;
+	return 2 * t;
 }
 
-//начальное условие
+double U(int j, int k) {
+	return U_k[j + 1] * (sigma + b * teta * teta / 2 / h) + U_k[j] * (-2 * sigma + 2 + c * teta * teta) +
+		U_k[j - 1] * (sigma - b * teta * teta / 2 / h) - U_km1[j] + teta * teta * f(j * h, teta * k);
+}
+
+//первое начальное условие
 double begU(double x) {
-	return sin(x);
+	return 0;
+}
+//второе начальное условие
+double dbegU(double x) {
+	return 0.01 * cos(x);
 }
 //правое граничное условие
 double RightU(double t) {
@@ -38,14 +157,14 @@ double RightU(double t) {
 }
 //левое граничное условие
 double LeftU(double t) {
-	//return alpha*(U_kp1[1]-U_kp1[0]
 	return 0;
 }
 
 double AnalitRew(double x,double t) {
 
-	return exp(-a * t) * sin(x);
+	return pow(x,2);
  }
+
 
 
 //void Gauss(int k, double Matrix[Nm][Nm + 1]) {
@@ -68,155 +187,49 @@ double AnalitRew(double x,double t) {
 //		Gauss(k + 1, Matrix);
 //	}
 //}
+//
+//void vyvod(double Matr[Nm][Nm + 1]) {
+//	for (int i = 0; i < Nm; i++) {
+//		for (int j = 0; j < Nm + 1; j++) {
+//			cout << Matr[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//}
 
-void vyvod(double Matr[Nm][Nm + 1]) {
-	for (int i = 0; i < Nm; i++) {
-		for (int j = 0; j < Nm + 1; j++) {
-			cout << Matr[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
+//явная схема
 void yavnaya() {
 	cout << "sigma: " << sigma << endl;
+	//k=0
 	for (int i = 0; i <= N; i++) {
-		U_k[i] = begU(i * h);
+		U_km1[i] = begU(i * h);
 	}
 	U_k[0] = LeftU(0);
 	U_k[N] = RightU(0);
-	for (int k = 1; k <= K; k++) {
+	//k=1
+	for (int i = 0; i <= N; i++) {
+		U_k[i] = begU(i * h) + dbegU(i * h) * teta;
+	}
+	U_k[0] = LeftU(teta);
+	U_k[N] = RightU(teta);
+	for (int k = 2; k <= K; k++) {
 		for (int j = 1; j <= N - 1; j++) {
-			U_k[j] = U(j);
-			//U_kLANA[j] = AnalitRew(j*h, T);
-			
+			U_kp1[j] = U(j, k);
+		}
+		for (int j = 1; j <= N - 1; j++) {
+			U_km1[j] = U_k[j];
+			U_k[j] = U_kp1[j];
 		}
 	}
 	cout << "yavnaya: " << endl;
-	ofstream f1("parabolich_yavn.txt", ios_base::trunc);
+	ofstream f1("hiperbol_yavn.txt", ios_base::trunc);
 	for (int i = 0; i <= N; i++) {
-		f1 << i * h << " " << U_k[i] << " " << AnalitRew(i * h, T) << endl;
-		cout << i * h << " " << U_k[i] << " " << AnalitRew(i * h, T) << endl;
+		f1 << i * h << "\t" << U_k[i] << "\t" << AnalitRew(i * h, T) << endl;
+		cout << i * h << "\t" << U_k[i] << "\t" << AnalitRew(i * h, T) << endl;
 	}
 	f1.close();
 }
 
-
-
-
-////////////////////////////////////////////////////////////////////////////
-//
-//using namespace std;
-//const int N = 40, K = 100;
-//const int Nm = N - 1;
-//double L = 5, T = 5,
-//h = L / N, teta = T / K;
-//double a = 1, b = 1, c = 1;
-//double U_k[N + 1], U_kp1[N + 1], U_km1[N + 1];   //матрицы для явной схемы
-//double A[Nm][Nm + 1];
-//double sigma = a * a * teta * teta / h / h;
-//
-///*
-//U_tt=a^2*U_xx+b*U_x+c*U+f(x,t)
-//*/
-//
-////alpha - коэффициент перед производной в левом условии
-////betha - коэффициент перед функцией в левом условии
-//double alpha = 1, betha = 1;
-////gamma - коэффициент перед производной в правом условии
-////delta - коэффициент перед функцией в правом условии
-//double gamma = 1, delta = 1;
-//
-//double f(double x, double t) {
-//	//return 0;
-//	return 2 * t;
-//}
-//
-//double U(int j, int k) {
-//	return U_k[j + 1] * (sigma + b * teta * teta / 2 / h) + U_k[j] * (-2 * sigma + 2 + c * teta * teta) +
-//		U_k[j - 1] * (sigma - b * teta * teta / 2 / h) - U_km1[j] + teta * teta * f(j * h, teta * k);
-//}
-//
-////первое начальное условие
-//double begU(double x) {
-//	return 0;
-//}
-////второе начальное условие
-//double dbegU(double x) {
-//	return 0.01 * cos(x);
-//}
-////правое граничное условие
-//double RightU(double t) {
-//	return 0;
-//}
-////левое граничное условие
-//double LeftU(double t) {
-//	return 0;
-//}
-//
-////void Gauss(int k, double Matrix[Nm][Nm + 1]) {
-////	if (Matrix[k][k] != 1) {
-////		double T = Matrix[k][k];
-////		for (int j = k; j < Nm + 1; j++) {//нормирование строки
-////			Matrix[k][j] = Matrix[k][j] / T;
-////		}
-////	}
-////	for (int i = 0; i < Nm; i++) { //проходим по столбцу
-////		if ((Matrix[i][k] != 0) & (i != k)) {
-////			double T = Matrix[i][k];
-////			Matrix[i][k] = 0;
-////			for (int j = k + 1; j < Nm + 1; j++) { //проходим по двум строкам и вычитаем их
-////				Matrix[i][j] -= Matrix[k][j] * T;
-////			}
-////		}
-////	}
-////	if (k < Nm - 1) {
-////		Gauss(k + 1, Matrix);
-////	}
-////}
-////
-////void vyvod(double Matr[Nm][Nm + 1]) {
-////	for (int i = 0; i < Nm; i++) {
-////		for (int j = 0; j < Nm + 1; j++) {
-////			cout << Matr[i][j] << " ";
-////		}
-////		cout << endl;
-////	}
-////}
-//
-////явная схема
-//void yavnaya() {
-//	cout << "sigma: " << sigma << endl;
-//	//k=0
-//	for (int i = 0; i <= N; i++) {
-//		U_km1[i] = begU(i * h);
-//	}
-//	U_k[0] = LeftU(0);
-//	U_k[N] = RightU(0);
-//	//k=1
-//	for (int i = 0; i <= N; i++) {
-//		U_k[i] = begU(i * h) + dbegU(i * h) * teta;
-//	}
-//	U_k[0] = LeftU(teta);
-//	U_k[N] = RightU(teta);
-//	for (int k = 2; k <= K; k++) {
-//		for (int j = 1; j <= N - 1; j++) {
-//			U_kp1[j] = U(j, k);
-//		}
-//		for (int j = 1; j <= N - 1; j++) {
-//			U_km1[j] = U_k[j];
-//			U_k[j] = U_kp1[j];
-//		}
-//	}
-//	cout << "yavnaya: " << endl;
-//	ofstream f1("hiperbol_yavn.txt", ios_base::trunc);
-//	for (int i = 0; i <= N; i++) {
-//		f1 << i * h << " " << U_k[i] << endl;
-//		cout << i * h << " " << U_k[i] << endl;
-//	}
-//	f1.close();
-//}
-//
 
 ///////////////////////////////////////
 
